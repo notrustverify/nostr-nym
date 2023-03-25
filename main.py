df38f5e1adc1bf9c13c2b7ca2dcda60e412d3b8f
cgi-bin <s@ono.re>
2023-03-25 11:39:01 +0100

    fix: wait for nym-client to start

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import nym
 if __name__ == '__main__':
 
     # wait for nym-client to start
+    print("Start nostr-nym proxy, waiting for nym-client to start")
     time.sleep(10)
 
     nym.Serve()
